a018e010e118c4eab3df02e2d0579cc34f97e436
Jatinnsharma <jatinsharma2231@gmail.com>
2024-10-29 06:07:37 +0530

    middleware session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useCurrect } from "@/features/auth/api/user-current";
+import { useRouter } from "next/navigation"
+import { useEffect } from "react";
 
 
 const Home = () => {
+  const router = useRouter()
+  const {data,isLoading} = useCurrect();
+  const {mutate} = useLogout()
+
+  useEffect(()=>{
+    if(!data && !isLoading){
+      router.push("/sign-in")
+    }
+  },[data])
+
   return (
     <div>
       <Button>Primary</Button>
@@ -15,6 +31,10 @@ const Home = () => {
       <p className="text-red-500">
         Jatin
       </p>
+
+      <Button onClick={()=>mutate()}>
+        Logout
+      </Button>
     </div>
   )
 }
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -6,8 +6,17 @@ import { createAdminClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
 import {deleteCookie, setCookie} from "hono/cookie"
 import { AUTH_COOKIE } from "../constansts";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 const app = new Hono()
+    .get(
+        "/current",
+        sessionMiddleware,
+        (c)=>{
+        const user = c.get("user")
+
+        return c.json({data:user})
+    })
     .post(
         "/login",
         zValidator("json",loginSchema),
@@ -62,8 +71,12 @@ const app = new Hono()
     })
     .post(
         "/logout",
-        (c)=>{
+        sessionMiddleware,
+       async (c)=>{
+        const account = c.get("account")
+        
         deleteCookie(c,AUTH_COOKIE);
+        await account.deleteSession("current");
 
         return c.json({success:true})
     });
